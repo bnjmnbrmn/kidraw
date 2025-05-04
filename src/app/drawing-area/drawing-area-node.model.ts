5bdda0121093886d782c9c2c65f73477da223c9e
Benjamin Berman <baberman@mmm.com>
2025-05-04 16:13:21 -0400

    Add nodes, zoom, pan

--- src/app/drawing-area/drawing-area-node.model.ts
+++ src/app/drawing-area/drawing-area-node.model.ts
@@ -1,194 +1,63 @@
-
-export interface NodeParams {
-  ctx: CanvasRenderingContext2D;
-  x: number;
-  y: number;
-  width?: number;
-  height?: number;
-  labelText?: string;
-  isSelected?: boolean;
-}
+import Konva from 'konva';
+import Group = Konva.Group;
+import Rect = Konva.Rect;
+import Text = Konva.Text;
+import Layer = Konva.Layer;
 
 
-interface Drawable {
-  leftBoundLogical: number,
-  rightBoundLogical: number,
-  topBoundLogical: number,
-  bottomBoundLogical: number,
-  widthLogical: number,
-  heightLogical: number
-}
-
-export class DANode implements Drawable {
-
-  get leftBoundLogical(): number {
-    return this.xLogical - this.widthLogical / 2
-  }
-
-  get rightBoundLogical(): number {
-    return this.xLogical + this.widthLogical / 2
-  }
-
-  get topBoundLogical(): number {
-    return this.yLogical - this.heightLogical / 2
-  }
-
-  get bottomBoundLogical(): number {
-    return this.xLogical + this.heightLogical / 2
-  }
-
-
-  get label(): DANodeLabel {
-    return this._label;
-  }
-
-  set label(value: DANodeLabel) {
-    this._label = value;
-  }
+export class DANode extends Group {
   get isSelected(): boolean {
     return this._isSelected;
   }
 
   set isSelected(value: boolean) {
     this._isSelected = value;
+    this.rect.strokeWidth(this.strokeWidth());
   }
 
-  private ctx: CanvasRenderingContext2D;
-  private xLogical: number;
-  private yLogical: number;
-  private minWidthLogical: number;
-  private minHeightLogical: number;
-  private _label: DANodeLabel;
-  private _isSelected: boolean;
-
-  constructor(nodeParams: NodeParams) {
-    this.ctx = nodeParams.ctx;
-    this.xLogical = nodeParams.x;
-    this.yLogical = nodeParams.y;
-    this.minWidthLogical = nodeParams.width ?? 100;
-    this.minHeightLogical = nodeParams.height ?? 100;
-    this._label = new DANodeLabel(this.ctx, nodeParams.labelText ?? '');
-    this._isSelected = nodeParams.isSelected ?? false;
+  private strokeWidth() {
+    return this._isSelected ? 4 : 2;
   }
 
-  draw() {
-
-
-
-    if (this._isSelected) {
-      this.ctx.lineWidth = 3;
-    } else {
-      this.ctx.lineWidth = 1;
-    }
-
-
-    this.ctx.strokeRect(this.xLogical - this.widthLogical / 2, this.yLogical - this.heightLogical / 2, this.widthLogical, this.heightLogical);
-
-    this._label.draw(this.xLogical, this.yLogical);
+  get rect(): Rect {
+    return this._rect;
+  }
+  get label(): Text {
+    return this._label;
   }
 
-  get heightLogical() {
-    return Math.max(this.minHeightLogical, Math.ceil(this._label.heightLogical / 50.0) * 100.0);
+  private readonly _rect: Rect;
+  private readonly _label: Text;
+  private _isSelected: boolean = true;
+
+
+  constructor(x: number, y: number, initialText: string) {
+    super({x, y});
+    const width = 100;
+    const height = 100;
+    this._rect = new Rect({
+      width: width,
+      height: height,
+      fill: 'white',
+      stroke: 'black',
+      strokeWidth: this.strokeWidth(),
+      // cornerRadius: 5,
+      offsetX: width/2,
+      offsetY: height/2,
+    });
+
+    this._label = new Text({
+      text: initialText,
+      fontSize: 16,
+      width: width,
+      height: height,
+      align: 'center',
+      verticalAlign: 'middle',
+      offsetX: width/2,
+      offsetY: height/2,
+    });
+    this.add(this._rect);
+    this.add(this._label);
   }
 
-  get widthLogical() {
-    return Math.max(this.minWidthLogical, Math.ceil(this._label.widthLogical / 50.0) * 100.0);
-  }
-}
-
-abstract class AbstractDrawable implements Drawable {
-    abstract leftBoundLogical: number;
-    abstract rightBoundLogical: number;
-    abstract topBoundLogical: number;
-    abstract bottomBoundLogical: number;
-    get widthLogical(): number {
-      return this.rightBoundLogical - this.leftBoundLogical;
-    }
-    get heightLogical(): number {
-      return this.bottomBoundLogical - this.topBoundLogical;
-    };
-
-}
-
-export class DANodeLabel extends AbstractDrawable {
-  private _text: string;
-  private ctx: CanvasRenderingContext2D;
-
-  get text() {
-    return this._text;
-  }
-
-  set text(value: string) {
-    this._text = value;
-  }
-
-  constructor(ctx: CanvasRenderingContext2D, text: string) {
-    super();
-    this._text = text;
-    this.ctx = ctx;
-    //todo handle multi-line text
-  }
-
-  get leftBoundLogical(): number {
-    let lbl!:number;
-    this.doInContext(() => {
-      lbl = this.ctx.measureText(this.text).actualBoundingBoxLeft;
-      }
-    );
-    return lbl;
-  }
-
-  get rightBoundLogical(): number {
-    let rbl!:number;
-    this.doInContext(() => {
-        rbl = this.ctx.measureText(this.text).actualBoundingBoxRight;
-      }
-    );
-    return rbl;
-  };
-
-  get topBoundLogical(): number {
-    let tbl!:number;
-    this.doInContext(() => {
-        tbl = this.ctx.measureText(this.text).actualBoundingBoxAscent;
-      }
-    );
-    return tbl;
-  }
-
-  get bottomBoundLogical(): number {
-    let bbl!:number;
-    this.doInContext(() => {
-        bbl = this.ctx.measureText(this.text).actualBoundingBoxDescent;
-      }
-    );
-    return bbl;
-  };
-
-
-  private font = "1em Arial";
-  private textAlign: CanvasTextAlign = "center";
-  private textBaseline: CanvasTextBaseline = "middle"
-
-  draw(x: number, y: number) {
-    this.doInContext(() => {
-      this.ctx.fillText(this.text, x, y);
-    })
-  }
-
-  private doInContext(toDo: () => void) {
-    const originalFont = this.ctx.font;
-    const originalTextAlign = this.ctx.textAlign;
-    const originalTextBaseline = this.ctx.textBaseline;
-
-    this.ctx.font = this.font;
-    this.ctx.textAlign = this.textAlign;
-    this.ctx.textBaseline = this.textBaseline;
-
-    toDo();
-
-    this.ctx.font = originalFont;
-    this.ctx.textAlign = originalTextAlign;
-    this.ctx.textBaseline = originalTextBaseline;
-  }
 }
--- src/app/drawing-area/drawing-area.component.ts
+++ src/app/drawing-area/drawing-area.component.ts
@@ -3,44 +3,10 @@ import {Observable} from 'rxjs';
 import {DACommand} from './command.model';
 import {DANode} from './drawing-area-node.model';
 import {DANotification} from './da-notification.model';
-
-export class DACrosshairs {
-  private hidden: boolean = false;
-
-  constructor(public ctx: CanvasRenderingContext2D, public x: number, public y: number) {
-  }
-
-  hide() {
-    this.hidden = true;
-  }
-
-  show() {
-    this.hidden = false;
-  }
-
-  draw() {
-    const ctx = this.ctx;
-    if (!this.hidden) {
-      const originalStrokeStyle = ctx.strokeStyle;
-      const originalLineWidth = ctx.lineWidth;
-
-      ctx.strokeStyle = 'rgba(0,0,0,0.5)'
-      ctx.lineWidth = 2;
-
-      ctx.beginPath()
-      ctx.moveTo(this.x - 20, this.y);
-      ctx.lineTo(this.x + 20, this.y);
-      ctx.stroke()
-      ctx.beginPath()
-      ctx.moveTo(this.x, this.y - 20);
-      ctx.lineTo(this.x, this.y + 20);
-      ctx.stroke()
-
-      ctx.strokeStyle = originalStrokeStyle;
-      ctx.lineWidth = originalLineWidth;
-    }
-  }
-}
+import Konva from 'konva';
+import {DACrosshairs} from './da-crosshairs';
+import Layer = Konva.Layer;
+import Stage = Konva.Stage;
 
 @Component({
   selector: 'app-drawing-area',
@@ -54,11 +20,8 @@ export class DrawingAreaComponent implements AfterViewInit {
   private componentNE = this.componentER.nativeElement;
   @ViewChild('mainDrawingArea') private mainDrawingAreaER!: ElementRef;
   private canvas!: HTMLCanvasElement;
-  private ctx!: CanvasRenderingContext2D;
 
   private daNodes: DANode[] = [];
-  // private crosshairsX!: number;
-  // private crosshairsY!: number;
   private crosshairs!: DACrosshairs;
 
   private resizeObserver!: ResizeObserver;
@@ -69,48 +32,41 @@ export class DrawingAreaComponent implements AfterViewInit {
   @Output() daOut = new EventEmitter<DANotification>()
   private panX: number = 0;
   private panY: number = 0;
+  private crosshairsLayer!: Layer;
+  private mainLayer!: Layer;
+  private stage!: Stage;
 
 
   ngAfterViewInit(): void {
-    this.canvas = this.mainDrawingAreaER.nativeElement as HTMLCanvasElement;
-    this.canvas.height = this.componentNE.offsetHeight;
-    this.canvas.width = this.componentNE.offsetWidth;
-    this.ctx = this.canvas.getContext("2d")!;
+    this.stage = new Stage({
+      container: 'mainDrawingArea',
+      width: this.componentNE.offsetWidth,
+      height: this.componentNE.offsetHeight,
+    });
+    this.stage.container().style.backgroundColor = 'white';
 
-    this.crosshairs = new DACrosshairs(this.ctx, this.canvas.width / 2, this.canvas.height / 2);
 
-    this.redraw();
+    this.mainLayer = new Layer();
+    this.mainLayer.move(
+      {x: this.stage.width() / 2, y: this.stage.height() / 2}
+    );
+    this.stage.add(this.mainLayer);
+
+    this.crosshairsLayer = new Layer();
+    this.crosshairs = new DACrosshairs();
+    this.crosshairsLayer.add(this.crosshairs);
+    this.crosshairsLayer.move(
+      {x: this.stage.width() / 2, y: this.stage.height() / 2}
+    );
+    this.stage.add(this.crosshairsLayer);
 
     this.commands.subscribe(this.handleCommands.bind(this));
 
     this.resizeObserver = new ResizeObserver(entries => {
-      for (let entry of entries) {
-        // console.log("entry: entry");
-        // const { width, height } = entry.contentRect;
-        // console.log(`New Size - Width: ${width}, Height: ${height}`);
-        this.canvas.height = this.componentNE.offsetHeight;
-        this.canvas.width = this.componentNE.offsetWidth;
-        this.redraw();
-      }
+      this.stage.width(this.componentNE.offsetWidth);
+      this.stage.height(this.componentNE.offsetHeight);
     });
     this.resizeObserver.observe(this.componentNE);
-  }
-
-  private redraw() {
-
-    let ctx = this.ctx;
-
-    ctx.resetTransform();
-    ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-    this.crosshairs.draw();
-
-    ctx.translate(this.panX + this.canvas.width/2, this.panY + this.canvas.height/2);
-    ctx.scale(this.scale, this.scale);
-
-    for (const node of this.daNodes) {
-      node.draw();
-    }
-
   }
 
   private handleCommands(command: DACommand) {
@@ -118,31 +74,51 @@ export class DrawingAreaComponent implements AfterViewInit {
     console.log("Command: " + JSON.stringify(command));
     switch (command.kind) {
       case "move-cursor-left":
-        this.crosshairs.x -= 50;
+        if (this.crosshairs.x() > -this.stage.width() / 2 + 60)
+          this.crosshairs.move({x: -50, y: 0});
+        else
+          this.mainLayer.move({x: 50, y: 0})
         break;
       case "move-cursor-down":
-        this.crosshairs.y += 50;
+        console.log(this.crosshairs.y());
+        if (this.crosshairs.y() < this.stage.height() / 2 - 60)
+          this.crosshairs.move({x: 0, y: 50});
+        else
+          this.mainLayer.move({x: 0, y: -50})
         break;
       case "move-cursor-right":
-        this.crosshairs.x += 50;
+        if (this.crosshairs.x() < this.stage.width() / 2 - 60)
+          this.crosshairs.move({x: 50, y: 0});
+        else
+          this.mainLayer.move({x: -50, y: 0})
         break;
       case "move-cursor-up":
-        this.crosshairs.y -= 50;
+        if (this.crosshairs.y() > -this.stage.height() / 2 + 60)
+          this.crosshairs.move({x: 0, y: -50});
+        else
+          this.mainLayer.move({x: 0, y: 50})
         break;
       case "create-new-node":
         console.log("creating new node");
-        let daNode = new DANode({ctx: this.ctx, x: this.physicalToLogicalX(this.crosshairs.x), y: this.physicalToLogicalY(this.crosshairs.y), isSelected: true});
+        console.log("this.mainLayer.x(): ", this.mainLayer.x());
+        console.log("this.crosshairs.getAbsolutePosition(this.crosshairsLayer).x", this.crosshairs.getAbsolutePosition(this.crosshairsLayer).x)
+        let daNode = new DANode(
+          (this.crosshairs.getAbsolutePosition(this.crosshairsLayer).x - (this.mainLayer.x() - this.stage.width()/2)) / this.mainLayer.scaleX(),
+          (this.crosshairs.getAbsolutePosition(this.crosshairsLayer).y - (this.mainLayer.y() - this.stage.height()/2)) / this.mainLayer.scaleY(),
+          "");
+        this.mainLayer.add(daNode);
         this.daNodes.push(daNode);
         this.daOut.emit({kind: "started-label-editing-mode"})
         break
       case "insert-char":
         const key = command.value;
         this.getSelectedDANodes().forEach(daNode => {
-          daNode.label.text += key;
+          daNode.label.text(daNode.label.text() + key);
         })
         this.crosshairs.hide();
         break;
       case "exit-label-edit-mode":
+        console.log("case exit-label-edit-mode")
         this.crosshairs.show();
         this.getSelectedDANodes().forEach(daNode => {
           daNode.isSelected = false;
@@ -150,20 +126,21 @@ export class DrawingAreaComponent implements AfterViewInit {
         break;
       case "toggle-item-selection":
         console.log("this.daNodes", this.daNodes)
-        const daNodesContainingPoint: DANode[] = this.getDANodesContainingCrosshairs();
-        console.log("daNodesContainingPoint", daNodesContainingPoint);
-        daNodesContainingPoint.forEach(daNode => daNode.isSelected = !daNode.isSelected);
+        const daNodesContainingCrosshairs: DANode[] = this.getDANodesContainingCrosshairs();
+        console.log("daNodesContainingCrosshairs", daNodesContainingCrosshairs);
+        daNodesContainingCrosshairs.forEach(daNode => daNode.isSelected = !daNode.isSelected);
         break;
-      case "zoom-in":
-        // this.ctx.translate(-this.crosshairs.x, -this.crosshairs.y);
-        // this.ctx.scale(4.0/3.0, 4.0/3.0);
-        // this.ctx.translate(this.crosshairs.x, this.crosshairs.y);
-        this.recenterCrossHairs();
-        this.scale = this.scale*3.0/2.0
+      case "zoom-in": {
+        const oldScale = this.mainLayer.scaleX();
+        const newScale = oldScale * 3.0 / 2.0;
+        this.mainLayer.scale({x: newScale, y: newScale});
+      }
         break;
-      case "zoom-out":
-        this.recenterCrossHairs();
-        this.scale = this.scale*2.0/3.0;
+      case "zoom-out": {
+        const oldScale = this.mainLayer.scaleX();
+        const newScale = oldScale * 2.0 / 3.0;
+        this.mainLayer.scale({x: newScale, y: newScale});
+      }
         break;
       case "pan-left":
         this.panX += 50;
@@ -179,36 +156,25 @@ export class DrawingAreaComponent implements AfterViewInit {
         break;
     }
 
-    this.redraw();
-
-  }
-
-  private physicalToLogicalY(physicalY: number) {
-    return physicalY - (this.panY + this.canvas.height / 2);
-  }
-
-  private physicalToLogicalX(physicalX: number) {
-    return physicalX - (this.panX + this.canvas.width / 2);
   }
 
   private getDANodesContainingCrosshairs() {
-    const crosshairsLogicalX = this.physicalToLogicalX(this.crosshairs.x);
-    const crosshairsLogicalY = this.physicalToLogicalY(this.crosshairs.y);
-    console.log("crosshairsLogicalX", crosshairsLogicalX);
-    console.log("crosshairsLogicalY", crosshairsLogicalY);
+    const {x: chX, y: chY} = this.crosshairs.getAbsolutePosition();
     return this.daNodes.filter(daNode => {
-      const leftBoundLogical = daNode.leftBoundLogical;
-      console.log("leftBoundLogical", leftBoundLogical)
-      const rightBoundLogical = daNode.rightBoundLogical;
-      console.log("rightBoundLogical", rightBoundLogical)
-      const topBoundLogical = daNode.topBoundLogical;
-      console.log("topBoundLogical", topBoundLogical)
-      const bottomBoundLogical = daNode.bottomBoundLogical;
-      console.log("bottomBoundLogical", bottomBoundLogical)
-      return leftBoundLogical < crosshairsLogicalX
-          && crosshairsLogicalX < rightBoundLogical
-          && topBoundLogical < crosshairsLogicalY
-          && crosshairsLogicalY < bottomBoundLogical;
+        const rect = daNode.rect;
+        const clientRect = rect.getClientRect(
+          // {relativeTo: this.crosshairs}
+        );
+        // console.log("clientRect", clientRect);
+
+        //todo test this after adding panning and zooming
+
+        // const {x: rectX, y: rectY} = rect.absolutePosition();
+
+        return clientRect.x < chX && chX < clientRect.x + clientRect.width
+          && clientRect.y < chY && chY < clientRect.y + clientRect.width;
+
+
       }
     );
   }
@@ -218,7 +184,7 @@ export class DrawingAreaComponent implements AfterViewInit {
   }
 
   private recenterCrossHairs() {
-    this.crosshairs.x = this.canvas.width / 2;
-    this.crosshairs.y =  this.canvas.height / 2;
+    // this.crosshairs.x = this.canvas.width / 2;
+    // this.crosshairs.y =  this.canvas.height / 2;
   }
 }
